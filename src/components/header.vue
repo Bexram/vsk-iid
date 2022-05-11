ffbf4fbbe5768689fc9522034380d3784912efd4
bexram <bexram33@mail.ru>
2022-05-11 17:41:44 +0300

    upd mobile menu

--- src/components/header.vue
+++ src/components/header.vue
@@ -16,6 +16,9 @@
                 <div v-on:click="toggle"  class="point"><a href="#programs" v-smooth-scroll>Программы страхования</a></div>
                 <div v-on:click="toggle"  class="point"><a href="#footer" v-smooth-scroll>Контакты</a></div>
             </div>
+            <div class="circle">
+            </div>
+            <div class="arrow">>></div>
         </div>
     </div>
 </template>
@@ -37,6 +40,30 @@
 </script>
 
 <style scoped>
+    .arrow {
+        position: absolute;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 18rem;
+        line-height: 83.5%;
+        /* or 251px */
+        color: transparent;
+        -webkit-text-stroke: 1px #5BD333B2;
+        letter-spacing: 0.01em;
+        top:13rem;
+        left:1rem;
+    }
+    .circle {
+        position: absolute;
+        width: 30rem;
+        height: 30rem;
+        left: -15rem;
+        top: 18rem;
+        z-index: 101;
+        background: rgba(91, 211, 51, 0.34);
+        filter: blur(3rem);
+        border-radius: 50%;
+    }
     a {
         color: black;
         text-decoration: none;
