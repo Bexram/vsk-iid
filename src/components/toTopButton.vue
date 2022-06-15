16daf41fb557f3a975f63a7198435a1da491e152
bexram <bexram33@mail.ru>
2022-06-15 09:31:01 +0300

    last issue

--- src/components/toTopButton.vue
+++ src/components/toTopButton.vue
@@ -10,7 +10,7 @@
             justify="center"
     >
         <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M13,20H11V8L5.5,13.5L4.08,12.08L12,4.16L19.92,12.08L18.5,13.5L13,8V20Z" />
+            <path fill="currentColor" d="M13,20H11V8L5.5,13.5L4.08,12.08L12,4.16L19.92,12.08L18.5,13.5L13,8V20Z"/>
         </svg>
     </v-btn>
 
@@ -21,16 +21,25 @@
         name: "toTopButton",
         methods: {
             toTop() {
-                window.scrollTo(0,0)
+                window.scrollTo(0, 0)
             }
         }
     }
 </script>
 
 <style scoped>
-.to__top{
-    position: fixed;
-    top: 90vh;
-    left: 85vw;
-}
+    .to__top {
+        position: fixed;
+        top: 90vh;
+        left: 95vw;
+    }
+
+    @media screen and (max-width: 650px) {
+        .to__top {
+            position: fixed;
+            top: 90vh;
+            left: 85vw;
+        }
+    }
+
 </style>
